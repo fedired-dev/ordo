8d2248efe42c6e46f6f8de321ed9b3c69313e317
Javier Caceres <novus@Javiers-MacBook-Pro.local>
2024-11-21 18:49:40 -0600

    update version

--- lib/ordo.ex
+++ lib/ordo.ex
@@ -1,5 +1,5 @@
 defmodule Ordo do
-  @version "1.0.0"
+  @version "1.0.1"
 
   def version, do: @version
 end
--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Pleroma.Mixfile do
       app: :pleroma,
       name: "Ordo",
       compat_name: "Pleroma",
-      version: version("1.0.0"),
+      version: version("1.0.1"),
       elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
