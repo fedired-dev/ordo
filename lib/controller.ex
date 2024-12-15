f89ee03d854f91b5a31bde6e13263f8a96615ecb
Javier Caceres <novus@MacBook-Pro-de-Javier.local>
2024-12-14 20:35:34 -0600

    ajuste de ruta de script

--- lib/controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule MiApp.Controller do
-  use MiAppWeb, :controller
-
-  def index(conn, _params) do
-    version = Ordo.version()
-
-    render(conn, "index.html", version: version)
-  end
-end
